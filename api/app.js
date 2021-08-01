0d7c8b5aa4acfbc4966405c14070a77b3ea36b6a
Ali Haydar <ali.haydar85@gmail.com>
2021-08-01 12:16:06 +1200

    tidy up lambda handler and add comments

--- api/app.js
+++ api/app.js
@@ -1,14 +1,15 @@
-const { DynamoDB } = require('@aws-sdk/client-dynamodb');
-const { unmarshall } = require('@aws-sdk/util-dynamodb');
+const { DynamoDB } = require('@aws-sdk/client-dynamodb'); // importing library from aws-sdk that allows the interaction with dynamodb
+const { unmarshall } = require('@aws-sdk/util-dynamodb'); // importing unmarshall function, which converts a DynamoDB record into a JavaScript object
 
 exports.lambdaHandler = async (event, context) => {
   try {
     console.log('here is the event received', event);
-    const dynamodb = new DynamoDB({ region: 'us-east-1' });
+    const dynamodb = new DynamoDB({ region: 'us-east-1' }); // creating a new instance of DynamoDB
     const params = {
       TableName: 'companies',
     };
 
+    // if no query parameter was passed to the function, then update the dynamodb params to query all companies
     if (!event.queryStringParameters) {
       params.ExpressionAttributeValues = {
         ':companyType': {
@@ -16,8 +17,9 @@ exports.lambdaHandler = async (event, context) => {
         },
       };
       params.FilterExpression = 'CompanyType = :companyType';
-    } else {
-      console.log('did I get here?', event.queryStringParameters);
+    }
+    // if the companyId query paramater was passed, then update the dynamodb params to filter according that company exactly
+    else {
       params.ExpressionAttributeValues = {
         ':companyId': {
           S: `COMPANY#${event.queryStringParameters.companyId}`,
@@ -26,15 +28,18 @@ exports.lambdaHandler = async (event, context) => {
       params.FilterExpression = 'CompanyId = :companyId';
     }
 
-    const results = await dynamodb.scan(params);
+    const results = await dynamodb.scan(params); // get the results from dynamodb according to the previously set params
     console.log('results', results);
     let unmarshalledResults = [];
+
+    // unmarshall every record returned (convert it into a JS object)
     for (const item of results.Items) {
       const unmarshalledRecord = unmarshall(item);
       unmarshalledResults.push(unmarshalledRecord);
     }
 
     console.log('unmarshalled results', unmarshalledResults);
+
     return {
       statusCode: 200,
       headers: {
